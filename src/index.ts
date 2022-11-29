c2cb73248396327cb3d3a6017ae156618f865797
Carlos Ferreira <carlos@especializati.com.br>
2022-11-29 15:03:13 -0300

    feat: running server node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('started')
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.get('/', (request, response) => {
+  response.send("Server up 2")
+})
+app.listen(PORT, () => {
+  console.log(`server running in port  ${PORT}`)
+})
