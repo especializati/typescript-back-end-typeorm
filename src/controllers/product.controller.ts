936a714c89015a9468f8bb7f13bd2228071f8177
Carlos Ferreira <carlos@especializati.com.br>
2022-11-30 14:14:11 -0300

    feat: list all products database

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,9 +1,15 @@
 import {Request, Response} from 'express'
+import { Product } from '../entities/product.entity'
+import AppDataSource from '../connection'
 
 class ProductController {
-  findAll(request: Request, response: Response): Response {
+  async findAll(request: Request, response: Response): Promise<Response> {
+    const productRepository = AppDataSource.getRepository(Product)
+
+    const products = await productRepository.find()
+
     return response.status(200).send({
-      data: []
+      data: products
     })
   }
 }
