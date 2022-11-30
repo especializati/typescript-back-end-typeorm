a2e02dec474c9d095e8cf3639c7a746833d271ac
Carlos Ferreira <carlos@especializati.com.br>
2022-11-30 14:32:35 -0300

    feat: store product

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,8 +1,14 @@
-import {Request, Response} from 'express'
+import {Request, response, Response} from 'express'
 import { Product } from '../entities/product.entity'
 import AppDataSource from '../connection'
+import { Repository } from 'typeorm'
 
 class ProductController {
+  private productRepository: Repository<Product>
+
+  constructor() {
+    this.productRepository = AppDataSource.getRepository(Product)
+  }
   async findAll(request: Request, response: Response): Promise<Response> {
     const productRepository = AppDataSource.getRepository(Product)
 
@@ -10,6 +16,20 @@ class ProductController {
 
     return response.status(200).send({
       data: products
+    })
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const productRepository = AppDataSource.getRepository(Product)
+
+    const product = new Product
+    product.name = "Prod 1"
+    product.weight = 90
+    product.description = 'desc prod'
+    const productDb = await productRepository.save(product)
+
+    return response.status(201).send({
+      data: productDb
     })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import productController from './controllers/product.controller'
 const PORT = process.env.PORT || 8080
 
 const app = express()
+app.post('/api/products', productController.create)
 app.get('/api/products', productController.findAll)
 app.get('/', (request, response) => {
   response.send("Server up 2")
